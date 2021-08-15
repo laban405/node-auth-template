4ef484faaf274e006a8acd91e7b57cb9a25aee72
laban405 <laban405@gmail.com>
2021-08-15 11:34:50 +0300

    initial commit

---
+++ src/routes/index.js
@@ -0,0 +1,5 @@
+import { testRoute } from "./testRoute";
+import { signUpRoute } from "./signUpRoute";
+import { logInRoute } from "./loginRoute";
+import { updateUserInfoRoute } from "./updateUserInfoRoute";
+export const routes = [signUpRoute, logInRoute,updateUserInfoRoute, testRoute];
